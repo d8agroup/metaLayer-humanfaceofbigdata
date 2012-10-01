2e806b54f5c74abdcff9b4400986e596ed9e5663
Matthew Griffiths <mg@metalayer.com>
2012-10-01 15:08:05 +0100

    More stuff

--- hfobd/settings.py
+++ hfobd/settings.py
@@ -184,7 +184,7 @@ LOGGING = {
 
 SENTRY_DSN = 'http://6cd19fe54e974d8c812768607612c1a0:6e6569fb84f94b41ab37e1a18e56c600@108.166.111.61:9000/4'
 
-SOLR = Solr('http://hfobd.metalayer.com:8983/solr/collection1')
+SOLR = Solr('http://hfobd.metalayer.com:8983/solr/core1')
 
 
 import socket
