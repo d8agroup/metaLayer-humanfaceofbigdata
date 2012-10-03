839df13fd30048a5f3b72f4bceb7eb101691aec8
Matthew Griffiths <mg@metalayer.com>
2012-10-03 15:24:15 +0100

    added login etc

--- hfobd/middleware/loggedinmiddleware.py
+++ hfobd/middleware/loggedinmiddleware.py
@@ -3,10 +3,10 @@ from django.shortcuts import redirect
 
 class LoggedInMiddleware(object):
     def process_request(self, request):
-        if request.path.lower() == '/landing':
+        if request.path.lower() in ['/splash', '/login']:
             return None
         if not settings.LOGIN_REQUIRED:
             return None
         if request.user.is_authenticated():
             return None
-        return redirect('/landing')
+        return redirect('/splash')
